b1dbbdfbf12c174cad223afaf712a7b5e714412d
owenroberts <obroberts@gmail.com>
2021-09-17 15:47:50 -0400

    initial commit

---
+++ sym.js
@@ -0,0 +1,8 @@
+import SnakeRenderer from './lib/SnakeRenderer.js';
+import SnakeShadows from './lib/SnakeShadows.js';
+// const snakeOne = new SnakeRenderer('snake-one', '../models/teddy-snake.glb');
+
+
+const snakeOne = new SnakeShadows('snake-one', '../models/teddy-snake.glb');
+
+
